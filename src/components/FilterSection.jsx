24cf03489bf6dbc6b935ea8ad338087ffdaf1a7f
anshumaanbaaghi <ganumumbai067@gmail.com>
2024-05-03 00:13:00 +0530

    feat: Configured multi-choice filter options for data filtering

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -1,10 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { MultipleSelect } from "./MultipleSelect";
-
+import { Box } from "@mui/material";
+const roleOptions = [
+  { label: "Option 1" },
+  { label: "Option 2" },
+  { label: "Option 3" },
+  { label: "Option 4" },
+  { label: "Option 5" },
+];
+const noOfEmployeesOptions = [
+  { label: "1-10", min: 1, max: 10 },
+  { label: "11-20", min: 11, max: 20 },
+  { label: "21-50", min: 21, max: 50 },
+  { label: "51-100", min: 51, max: 100 },
+  { label: "101-200", min: 101, max: 200 },
+  { label: "201-500", min: 201, max: 500 },
+  { label: "500+", min: 501 },
+];
+const experienceOptions = [
+  { label: "1" },
+  { label: "2" },
+  { label: "3" },
+  { label: "4" },
+  { label: "5" },
+  { label: "6" },
+  { label: "7" },
+  { label: "8" },
+  { label: "9" },
+  { label: "10" },
+];
+const RemoteOptions = [
+  { label: "Remote" },
+  { label: "Hybrid" },
+  { label: "In-Office" },
+  ,
+];
 export const FilterSection = () => {
+  const [selectedRoles, setSelectedRoles] = useState([]);
+  const [selectedNoOfEmployees, setSelectedNoOfEmployees] = useState([]);
+  const [selectedExperience, setSelectedExperience] = useState("");
+  const [selectedRemote, setSelectedRemote] = useState([]);
   return (
-    <div>
-      <MultipleSelect />
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "10px",
+        alignItems: "end",
+      }}
+    >
+      <MultipleSelect
+        options={roleOptions}
+        selectedOptions={selectedRoles}
+        setSelectedOptions={setSelectedRoles}
+        placeholder="Roles"
+        label="Roles"
+      />
+      <MultipleSelect
+        options={noOfEmployeesOptions}
+        selectedOptions={selectedNoOfEmployees}
+        setSelectedOptions={setSelectedNoOfEmployees}
+        placeholder="Number Of Employees"
+        label="No Of Employees"
+      />
+      {/* <MultipleSelect
+        options={experienceOptions}
+        selectedOptions={selectedExperience}
+        setSelectedOptions={setSelectedExperience}
+        placeholder="Experience"
+        label="Experience"
+      /> */}
+      <MultipleSelect
+        options={RemoteOptions}
+        selectedOptions={selectedRemote}
+        setSelectedOptions={setSelectedRemote}
+        placeholder="Remote"
+        label="Remote"
+      />
+    </Box>
   );
 };
--- src/components/MultipleSelect.jsx
+++ src/components/MultipleSelect.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
-import { Chip } from "@mui/material";
+import { Box, Chip } from "@mui/material";
 
-const options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"];
-
-export const MultipleSelect = () => {
+export const MultipleSelect = ({
+  options,
+  selectedOptions,
+  setSelectedOptions,
+  placeholder,
+  label,
+}) => {
   const [inputValue, setInputValue] = React.useState("");
   const [optionsFiltered, setOptionsFiltered] = React.useState(options);
-  const [selectedOptions, setSelectedOptions] = React.useState([]);
   console.log("selectedOptions:", selectedOptions);
 
   const handleInputChange = (event) => {
@@ -16,7 +19,7 @@ export const MultipleSelect = () => {
     setInputValue(query);
 
     const filteredOptions = options.filter((option) =>
-      option.toLowerCase().includes(query.toLowerCase())
+      option.label.toLowerCase().includes(query.toLowerCase())
     );
     setOptionsFiltered(filteredOptions);
   };
@@ -30,35 +33,41 @@ export const MultipleSelect = () => {
   };
 
   return (
-    <Autocomplete
-      multiple
-      value={selectedOptions}
-      onChange={handleSelectChange}
-      options={optionsFiltered}
-      getOptionLabel={(option) => option}
-      renderTags={(value, getTagProps) =>
-        value.map((option, index) => (
-          <Chip
-            className="multipleSelect-chip"
-            key={option}
-            label={option}
-            onDelete={() => {
-              const newValues = [...selectedOptions];
-              newValues.splice(index, 1);
-              handleSelectChange(null, newValues);
-            }}
-          />
-        ))
-      }
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          variant="outlined"
-          value={inputValue}
-          onChange={handleInputChange}
-          key={selectedOptions.map((option) => option).join("-")}
-        />
+    <Box sx={{ minWidth: "200px" }}>
+      {selectedOptions?.length > 0 && (
+        <Box sx={{ fontWeight: "400", fontSize: "14px" }}>{label}</Box>
       )}
-    />
+      <Autocomplete
+        multiple
+        value={selectedOptions}
+        onChange={handleSelectChange}
+        options={optionsFiltered}
+        getOptionLabel={(option) => option.label}
+        renderTags={(value, getTagProps) =>
+          value.map((option, index) => (
+            <Chip
+              className="multipleSelect-chip"
+              key={option.label}
+              label={option.label}
+              onDelete={() => {
+                const newValues = [...selectedOptions];
+                newValues.splice(index, 1);
+                handleSelectChange(null, newValues);
+              }}
+            />
+          ))
+        }
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder={selectedOptions?.length == 0 ? placeholder : ""}
+            key={selectedOptions.map((option) => option.label).join("-")}
+          />
+        )}
+      />
+    </Box>
   );
 };
